7ae94b9fc6b4ef37ec7ae6c1d89b9c101ccae2b7
daisy chepkemi <dchepkemoi408@gmail.com>
2020-11-06 11:41:22 +0300

    configured report generation

--- src/test/java/runners/TestRunners.java
+++ src/test/java/runners/TestRunners.java
@@ -6,9 +6,14 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
- features = "src/test/resources/Feature"
- ,glue={"stepDefinition"}
+@CucumberOptions( plugin ={
+						"pretty:pretty-report.log", 
+						"html:target/jsonReports/html-report.html",
+						"json:target/jsonReports/json-report.json" ,
+						"junit:target/jsonReports/junit-report.xml"
+						},
+				 features = "src/test/resources/Feature"
+				 ,glue={"stepDefinition"}
  )
 
  
