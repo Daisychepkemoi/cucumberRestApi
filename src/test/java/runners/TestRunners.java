739d3ffd1482e9fd0af8db9ee44481b99ce4a8f4
Daisy Chepkemoi <dchepkemoi408@gmail.com>
2021-02-18 19:13:57 +0300

    added tags to runner class

--- src/test/java/runners/TestRunners.java
+++ src/test/java/runners/TestRunners.java
@@ -14,7 +14,7 @@ import reportgenerate.GenerateNewReports;
 						},
 				 features = "src/test/resources/Feature",
 				 glue={"stepDefinition"},
-				 tags = ("@Comments")
+				 tags = ("@Posts")
 				//  glue = "fr.tlasnier.cucumber"
  )
 public class TestRunners {
